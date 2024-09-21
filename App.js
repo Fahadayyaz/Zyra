2bd19f4791af4c6eb21c299eb7930ab8842a1134
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-09-21 10:30:15 +0500

    fonts added with return

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import "react-native-gesture-handler";
 import React from "react";
+import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
@@ -9,6 +10,15 @@ import Login from "./screens/Login";
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    Poppins_SemiBold: require("../Zyra/assets/fonts/Poppins-SemiBold.ttf"),
+    PlusJakartaSans_Regular: require("../Zyra/assets/fonts/PlusJakartaSans-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
