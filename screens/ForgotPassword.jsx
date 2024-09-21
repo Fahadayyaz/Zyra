452eac51b9b3c53d2e5da8e43bccd4e4d6ef1bd7
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-09-21 17:00:09 +0500

    Update ForgotPassword.jsx

--- screens/ForgotPassword.jsx
+++ screens/ForgotPassword.jsx
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 
 const ForgotPassword = () => {
   return (
-    <View>
-      <Text>ForgotPassword</Text>
+    <View style={styles.mainContainer}>
+      <ExpoStatusBar style="auto" />
+      <View style={styles.contentContainer}></View>
     </View>
   );
 };
 
 export default ForgotPassword;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#f1f1f1",
+  },
+  contentContainer: {
+    width: "90%",
+    height: "100%",
+    alignSelf: "center",
+    backgroundColor: "lightblue",
+    marginTop: "10%",
+  },
+});
